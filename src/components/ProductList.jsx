3949a99c653976ae5c720b3208647782473f6b65
sck <whhan03@gmail.com>
2026-06-20 10:43:10 +0900

    상품 목록 테이블 생성

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -6,7 +6,35 @@ const ProductList = () => {
         <div>
             <h2>상품 리스트</h2>
             {/* { xxx.map( () => () ) } */}
-            <ul>
+            <table className='table-list'>
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>상품명</th>
+                        <th>가격</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product) => (
+                    <tr key={product.id}>
+                        <td>{product.id}</td>
+                        <td>
+                            <Link to = {`/products/${product.id}`}>
+                                {product.name}
+                            </Link>
+                        </td>
+                        <td>{product.price}</td>
+                    </tr>                        
+                    ))}
+                </tbody>
+            </table>
+            {/* 상품 등록 버튼 */}
+            <div className='btn-add'>
+                <Link to = '/add-product'>
+                    <button>상품 등록</button>
+                </Link>
+            </div>
+            {/* <ul>
                 {products.map( (product) => (
                     <li key={product.id}>
                         <h3>
@@ -16,7 +44,7 @@ const ProductList = () => {
                         <p>설명 : {product.description}</p>
                     </li>
                 ) )}
-            </ul>
+            </ul> */}
 
         </div>
     )    
